6bf1856c03fa13e282e030ad45188d1a512c0366
Crystal Zawedde <zaweddecrystalangella@gmail.com>
2026-02-11 13:29:07 +0200

    1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,37 +1,44 @@
+const App = (props) => {
+  const course = 'Half Stack application development'
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  const total = exercises1 + exercises2 + exercises3
+  return (
+    <div>
+      <Header course={course} />
+      <Content 
+      exercises1={exercises1}
+      exercises2={exercises2}
+      exercises3={exercises3}/>
+      <Total total={total} />
+    </div>
+  )
+}
 const Header =(props)=>{
   return(<h1>
     {props.course}
   </h1>)}
-
-  const Content =(props)=>{
-    return(
-      <div>
-        <p>{props.part1} {props.exercises1}</p>
-        <p>{props.part2} {props.exercises2}</p>
-        <p>{props.part3} {props.exercises3}</p>
-      </div>
-    )
-  }
-  const Total =(props)=>{
-    return(
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    )
-  }
-const App = (props) => {
-  const course = 'Half Stack Application Development'
-  const part1 = 'fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-    </div>
+const Part =(props)=>{
+  return(
+    <p>{props.name}: {props.exercises}</p>
   )
 }
+const Content=(props)=>{return(
+    <div>
+      <Part name ="fundamentals of React" exercises = {props.exercises1}/>
+      <Part name ="using props to pass data " exercises = {props.exercises2}/>
+      <Part name ="state of a component" exercises = {props.exercises3}/>
+
+
+  </div>)
+}
+
+const Total=(props)=>{
+  return(
+    <p>Number of exercises {props.total}</p>
+  )
+}
+ 
+
 export default App 
